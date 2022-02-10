915d6f4a0088b08df3301d6ad5a8cc675c93af93
gaeul <e0540@hanmail.net>
2022-02-10 21:05:17 +0900

    으악 제발....!!

--- sparta/settings.py
+++ sparta/settings.py
@@ -37,7 +37,8 @@ def get_secret(setting, secrets=secrets):
         error_msg = "Set the {} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
 
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = {{ secrets.SECRET_KEY }}
+    # get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
